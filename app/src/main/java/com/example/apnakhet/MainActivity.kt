19acf1e5773792e461bb1048214209d9296014b4
Shagufta Varsi <shagufta21comp@student.mes.ac.in>
2024-08-16 17:37:03 +0530

    LoginPage

--- app/src/main/java/com/example/apnakhet/MainActivity.kt
+++ app/src/main/java/com/example/apnakhet/MainActivity.kt
@@ -7,13 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.apnakhet.ui.theme.ApnaKhetTheme
 import com.example.apnakhet.ui.theme.login.LoginNavigation
 
